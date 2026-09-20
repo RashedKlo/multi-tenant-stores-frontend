05ff062b573e3d45c91cc9a455be6d1b708f688e
Rashed Klo <rashed.klo.dev@gmail.com>
2026-09-20 19:44:10 +0300

    fix(shared): update cache configuration

--- src/shared/config/cache.ts
+++ src/shared/config/cache.ts
@@ -1,15 +1,19 @@
-// shared/config/cache.ts
 export const CACHE_TAGS = {
   homeBanners: "home-banners",
   modules: "modules",
-  addresses:"addresses",
-  address:(id:string)=>"addresses"+id,
-  cart:"cart",
-  moduleDetail:(id:string)=>"modules"+id,
-  moduleStores:(id:string)=>"modules"+id+"stores",
+  addresses: "addresses",
+  address: (id: string) => `addresses${id}`,
+  cart: "cart",
+  profile: "profile",
+  storeDetail: (id: string) => `stores${id}`,
+  storeBanners: (id: string) => `stores${id}banners`,
+  storeSections: (id: string) => `stores${id}sections`,
+  sectionProducts: (id: string) => `sections${id}products`,
+  moduleDetail: (id: string) => `modules${id}`,
+  moduleStores: (id: string) => `modules${id}stores`,
 } as const;
 
 export const REVALIDATE = {
   hour: 3600,
-  minute:60
+  minute: 60,
 } as const;
